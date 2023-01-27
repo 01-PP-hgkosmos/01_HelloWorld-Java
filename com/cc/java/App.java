30caee7fe24f38456a9026c467bbd73ddac8b73b
Peter <schmidt.multimedia@gmail.com>
2023-01-27 13:52:06 +0100

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,10 +1,10 @@
 package com.cc.java;
 
 public class App {
-    
+  
     public static void main(String[] args) {
-       System.out.println("Hello, World!"); 
-    // System.out.println("12:55, Mittag!");
+      System.out.println("Hi");
+      System.out.println("TGIF, Mittag!");
     }
 
     
